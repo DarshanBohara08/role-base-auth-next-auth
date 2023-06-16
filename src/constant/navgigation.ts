90335671baf78eda308b2ecb1c17e58e5a57d8b0
darshanqubits21 <darshanqubits@gmail.com>
2023-06-16 22:20:59 +0545

    feat: create layout, admin and user page ui

---
+++ src/constant/navgigation.ts
@@ -0,0 +1,14 @@
+export const navigation = [
+  {
+    label: "Home",
+    link: "/",
+  },
+  {
+    label: "Admin",
+    link: "/admin",
+  },
+  {
+    label: "User",
+    link: "/user",
+  },
+];
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const IndexPage = () => (
+  <>
+    <p>This is home page</p>
+  </>
+);
+
+export default IndexPage;
